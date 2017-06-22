c143a91e3a9fcba3df2c875b61bd902943ae6571
Totti10as <stasma@gmail.com>
2017-06-22 15:38:27 +0300

    Created by Coding Ground Client

---
+++ New Project-20170622/main.sql
@@ -0,0 +1,26 @@
+-- BEGIN TRANSACTION;
+
+-- /* Create a table called NAMES */
+-- CREATE TABLE NAMES(Id integer PRIMARY KEY, Name text);
+
+-- /* Create few records in this table */
+-- INSERT INTO NAMES VALUES(1,'Tom');
+-- INSERT INTO NAMES VALUES(2,'Lucy');
+-- INSERT INTO NAMES VALUES(3,'Frank');
+-- INSERT INTO NAMES VALUES(4,'Jane');
+-- INSERT INTO NAMES VALUES(5,'Robert');
+-- COMMIT;
+
+-- /* Display all the records from the table */
+-- SELECT * FROM NAMES;
+
+/* Create new Table*/
+-- CREATE TABLE Usersname (
+--   PersonId int NOT NULL PRIMARY KEY,
+--   FirstName varchar(255),
+--   LastName varchar(255),
+--   Address varchar(255),
+--   City varchar(255)
+-- );
+
+INSERT INTO Usersname (Personid, FirstName, LastName, Address, City) VALUES (1, 'Stanislav', 'Masarsky', 'Waizman', 'Holon');
